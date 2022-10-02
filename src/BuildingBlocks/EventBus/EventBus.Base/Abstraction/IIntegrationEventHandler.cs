1597444ca798a33970636e0ceabea7e60de37899
Mehmet Efe Kahveci <efekhvci@hotmail.com>
2022-10-02 15:05:13 +0300

    Add SubscriptionManager Infrastructure and Implements

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Abstraction;
+
+public interface IIntegrationEventHandler<TIntegrationEvent> where TIntegrationEvent:IntegrationEvent
+{
+    Task Handle(TIntegrationEvent integrationEvent);
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Events;
+
+public class IntegrationEvent
+{
+    [JsonPropertyName("ID")]
+    public Guid Id { get; private set; }
+    [JsonPropertyName("DateTime")]
+
+    public DateTime CreatedDate { get; private set; }
+
+
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreatedDate = DateTime.UtcNow;
+    }
+    [JsonConstructor]
+    public IntegrationEvent(Guid guid, DateTime dateTime)
+    {
+        Id = guid;
+        CreatedDate = dateTime; 
+    }
+}
